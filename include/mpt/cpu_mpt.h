15f9f7e8ee8e05e6eace9cb4a345aaf4e87f9c5f
Yangshen Deng <yangshen.d@outlook.com>
2022-10-25 11:49:01 +0000

    update interface and format

--- include/mpt/cpu_mpt.h
+++ include/mpt/cpu_mpt.h
@@ -1,1 +1,10 @@
 #include "mpt/mpt.h"
+
+template <typename KeyT, typename ValueT>
+class CpuMPT : public MPT<KeyT, ValueT> {
+public:
+  void puts(const KeyT *keys, const ValueT *values, int n,
+            DeviceT device) final;
+  void gets(const KeyT *keys, ValueT *values, int n, DeviceT device) final;
+  void hash(char *bytes /* char[32] */, DeviceT device) final;
+};
--- include/mpt/mpt.h
+++ include/mpt/mpt.h
@@ -2,12 +2,9 @@
 
 #include "util/util.h"
 
-template <typename KeyT, typename ValueT>
-class MPT {
- public:
-  virtual void puts(const KeyT *keys, const ValueT *values, int n,
-                    DeviceT device) = 0;
-  virtual void gets(const KeyT *keys, ValueT *values, int n,
-                    DeviceT device) = 0;
-  virtual void hash(char (&bytes)[32], DeviceT device);  // 256 bits
+template <typename KeyT, typename ValueT> class MPT {
+public:
+  void puts(const KeyT *keys, const ValueT *values, int n, DeviceT device) = 0;
+  void gets(const KeyT *keys, ValueT *values, int n, DeviceT device) = 0;
+  void hash(char *bytes /* char[32] */, DeviceT device);
 };
