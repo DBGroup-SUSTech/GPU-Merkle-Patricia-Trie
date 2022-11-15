272fa49b1767a04f600fad4e5ee3f2e46723fd55
TKONIY <yangshen.d@outlook.com>
2022-11-15 03:36:00 +0000

    [feat] Add GPU timer

--- include/util/timer.h
+++ include/util/timer.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include "util/util.cuh"
 #include <chrono>
 
 namespace perf {
@@ -33,4 +33,34 @@ template <typename T> int CpuTimer<T>::get() {
   return std::chrono::duration_cast<T>(after_ - before_).count();
 }
 
+template <typename T> class GpuTimer : Timer {
+  static_assert(std::is_same_v<T, ms>);
+
+public:
+  GpuTimer(cudaStream_t stream = (cudaStream_t)0) : stream_(stream) {
+    CHECK_ERROR(cudaEventCreate(&start_));
+    CHECK_ERROR(cudaEventCreate(&stop_));
+  }
+
+public:
+  void start() final;
+  void stop() final;
+  int get() final;
+
+private:
+  cudaStream_t stream_;
+  cudaEvent_t start_;
+  cudaEvent_t stop_;
+};
+template <typename T> void GpuTimer<T>::start() {
+  CHECK_ERROR(cudaEventRecord(start_, stream_));
+}
+template <typename T> void GpuTimer<T>::stop() {
+  CHECK_ERROR(cudaEventRecord(stop_, stream_));
+}
+template <typename T> int GpuTimer<T>::get() {
+  float ms;
+  CHECK_ERROR(cudaEventElapsedTime(&ms, start_, stop_));
+  return static_cast<int>(ms);
+}
 } // namespace perf
