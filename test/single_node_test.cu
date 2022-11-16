9eb5c93b72b8b7c3df65d32f06aec4ff753b0ae9
TKONIY <yangshen.d@outlook.com>
2022-11-16 15:13:28 +0000

    Add single test

--- test/single_node_test.cu
+++ test/single_node_test.cu
@@ -3,11 +3,11 @@
 #include "util/util.cuh"
 int main() {
   uint32_t keys[]{0x0000b0a0};
-  uint8_t values[64]{};
-  for (int i = 0; i < 64; ++i) {
+  uint8_t values[800]{};
+  for (int i = 0; i < 800; ++i) {
     values[i] = 'a';
   }
-
+  GPUHashMultiThread::load_constants();
   // const int n = 1;
   // const uint8_t *keys_bytes = reinterpret_cast<const uint8_t *>(keys);
   // const uint8_t *values_bytes = reinterpret_cast<const uint8_t *>(values);
@@ -40,20 +40,36 @@ int main() {
 
   // single hash test
   uint8_t *d_value, *d_hash;
-  CHECK_ERROR(gutil::DeviceAlloc(d_value, 64));
-  CHECK_ERROR(gutil::CpyHostToDevice(d_value, values, 64));
+  CHECK_ERROR(gutil::DeviceAlloc(d_value, 800));
+  CHECK_ERROR(gutil::CpyHostToDevice(d_value, values, 800));
   CHECK_ERROR(gutil::DeviceAlloc(d_hash, 32));
   CHECK_ERROR(gutil::DeviceSet(d_hash, 0x00, 32));
 
-  // !! debug
-  gkernel::debug::calculate_one_hash<<<1, 32>>>(d_value, 64, d_hash);
+  // // !! debug
+  // gkernel::debug::calculate_one_hash<<<1, 32>>>(d_value, 800, d_hash);
 
-  CHECK_ERROR(cudaDeviceSynchronize());
+  // CHECK_ERROR(cudaDeviceSynchronize());
 
   uint8_t h_hash[32]{};
+  // CHECK_ERROR(gutil::CpyDeviceToHost(h_hash, d_hash, 32));
+  // printf("data is 0x");
+  // for (int i = 0; i < 800; ++i) {
+  //   printf("%02x", values[i]);
+  // }
+  // printf("\nhash is 0x");
+  // for (int i = 0; i < 32; ++i) {
+  //   printf("%02x", h_hash[i]);
+  // }
+  // printf("\n");
+
+  // CHECK_ERROR(gutil::DeviceSet(d_hash, 0x00, 32));
+
+  // call keccak
+  keccak_kernel<<<1, 32>>>((uint64_t *)d_value, (uint64_t *)d_hash, 800 * 8);
+
   CHECK_ERROR(gutil::CpyDeviceToHost(h_hash, d_hash, 32));
   printf("data is 0x");
-  for (int i = 0; i < 32; ++i) {
+  for (int i = 0; i < 800; ++i) {
     printf("%02x", values[i]);
   }
   printf("\nhash is 0x");
