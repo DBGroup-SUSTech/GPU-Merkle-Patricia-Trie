4040eafd5b98660073e226d1898f68f34d8f530e
ymx_lab_workstation <1105989949@qq.com>
2022-11-17 14:07:17 +0800

    hash function debug

--- test/hash_SIMD.cu
+++ test/hash_SIMD.cu
@@ -7,116 +7,123 @@
 #include <string>
 #include "hash/batch_mode_hash.cuh"
 
-#define DATA_INPUT_LENGTH 864
+#define DATA_INPUT_LENGTH 32
 #define MUL_FACTOR 1
 #define GEN_DATA_NUM 16
 #define GEN_DATA_MUL 20
 
-__global__ void cpy_data(uint64_t ** two, uint64_t * one, uint64_t ** hashtwo, uint64_t *hashone){
-  for (size_t i = 0; i < GEN_DATA_MUL*GEN_DATA_NUM; i++)
+__global__ void cpy_data(uint64_t **two, uint64_t *one, uint64_t **hashtwo, uint64_t *hashone)
+{
+  for (size_t i = 0; i < GEN_DATA_MUL * GEN_DATA_NUM; i++)
   {
-    two[i] = one + i*DATA_INPUT_LENGTH*MUL_FACTOR/8;
-    hashtwo[i] = hashone +i*4;
+    two[i] = one + i * DATA_INPUT_LENGTH * MUL_FACTOR / 8;
+    hashtwo[i] = hashone + i * 4;
   }
 }
 
-void call_keccak_batch_kernel(uint64_t * in, int * index, uint64_t * out, perf::CpuTimer<perf::ns> & ptimer, int data_num=32){
-  uint64_t ** d_data;
-  uint64_t ** out_hash;
-  uint64_t * data_l;
+void call_keccak_batch_kernel(uint64_t *in, int *index, uint64_t *out, perf::CpuTimer<perf::ns> &ptimer, int data_num = 32)
+{
+  uint64_t **d_data;
+  uint64_t **out_hash;
+  uint64_t *data_l;
   uint64_t *hash_l;
-  CUDA_SAFE_CALL(gutil::DeviceAlloc(data_l, GEN_DATA_MUL*GEN_DATA_NUM*DATA_INPUT_LENGTH*MUL_FACTOR/8));
-  CUDA_SAFE_CALL(gutil::DeviceAlloc(d_data, GEN_DATA_MUL*GEN_DATA_NUM));
-  CUDA_SAFE_CALL(gutil::DeviceAlloc(out_hash, GEN_DATA_MUL*GEN_DATA_NUM));
-  CUDA_SAFE_CALL(gutil::DeviceAlloc(hash_l,GEN_DATA_MUL*GEN_DATA_NUM*4));
-  CUDA_SAFE_CALL(gutil::DeviceSet(hash_l,0,GEN_DATA_MUL*GEN_DATA_NUM*4));
-  int * d_index;
+  CUDA_SAFE_CALL(gutil::DeviceAlloc(data_l, GEN_DATA_MUL * GEN_DATA_NUM * DATA_INPUT_LENGTH * MUL_FACTOR / 8));
+  CUDA_SAFE_CALL(gutil::DeviceAlloc(d_data, GEN_DATA_MUL * GEN_DATA_NUM));
+  CUDA_SAFE_CALL(gutil::DeviceAlloc(out_hash, GEN_DATA_MUL * GEN_DATA_NUM));
+  CUDA_SAFE_CALL(gutil::DeviceAlloc(hash_l, GEN_DATA_MUL * GEN_DATA_NUM * 4));
+  CUDA_SAFE_CALL(gutil::DeviceSet(hash_l, 0, GEN_DATA_MUL * GEN_DATA_NUM * 4));
+  int *d_index;
 
-  cpy_data<<<1,1>>>(d_data,data_l,out_hash,hash_l);
-  for (size_t i = 0; i < GEN_DATA_MUL*GEN_DATA_NUM; i++)
+  cpy_data<<<1, 1>>>(d_data, data_l, out_hash, hash_l);
+  for (size_t i = 0; i < GEN_DATA_MUL * GEN_DATA_NUM; i++)
   {
-    CUDA_SAFE_CALL(gutil::DeviceSet(hash_l,0,GEN_DATA_MUL*GEN_DATA_NUM*4));
+    CUDA_SAFE_CALL(gutil::DeviceSet(hash_l, 0, GEN_DATA_MUL * GEN_DATA_NUM * 4));
   }
 
-  CUDA_SAFE_CALL(gutil::DeviceAlloc(d_index,GEN_DATA_MUL*GEN_DATA_NUM));
-  CUDA_SAFE_CALL(gutil::CpyHostToDevice(data_l, in, GEN_DATA_MUL*GEN_DATA_NUM*DATA_INPUT_LENGTH*MUL_FACTOR/8));
-  CUDA_SAFE_CALL(gutil::CpyHostToDevice(d_index, index, GEN_DATA_MUL*GEN_DATA_NUM));
+  CUDA_SAFE_CALL(gutil::DeviceAlloc(d_index, GEN_DATA_MUL * GEN_DATA_NUM));
+  CUDA_SAFE_CALL(gutil::CpyHostToDevice(data_l, in, GEN_DATA_MUL * GEN_DATA_NUM * DATA_INPUT_LENGTH * MUL_FACTOR / 8));
+  CUDA_SAFE_CALL(gutil::CpyHostToDevice(d_index, index, GEN_DATA_MUL * GEN_DATA_NUM));
 
   ptimer.start();
   // keccak_kernel_batch_static<<<GEN_DATA_MUL,32*GEN_DATA_NUM>>>(d_data, out_hash, d_index, data_num);
-  keccak_kernel_batch<<<GEN_DATA_MUL, 32*GEN_DATA_NUM, 100*GEN_DATA_NUM*sizeof(uint64_t)>>>(d_data, out_hash,d_index,GEN_DATA_NUM,data_num);
+  keccak_kernel_batch<<<GEN_DATA_MUL, 32 * GEN_DATA_NUM, 100 * GEN_DATA_NUM * sizeof(uint64_t)>>>(d_data, out_hash, d_index, GEN_DATA_NUM, data_num);
   CUDA_SAFE_CALL(cudaDeviceSynchronize());
   ptimer.stop();
-  CUDA_SAFE_CALL(gutil::CpyDeviceToHost(out, hash_l, GEN_DATA_MUL*GEN_DATA_NUM*4));
+  CUDA_SAFE_CALL(gutil::CpyDeviceToHost(out, hash_l, GEN_DATA_MUL * GEN_DATA_NUM * 4));
 }
 
-void data_gen(const uint8_t *&values_bytes, int *&value_indexs, int n, int turn) {
+void data_gen(const uint8_t *&values_bytes, int *&value_indexs, int n, int turn)
+{
   // n = 1 << 16;
   std::random_device rd;
   std::mt19937 g(rd());
   std::uniform_int_distribution dist(0, 1 << 8);
   // generate random values
-  const int value_size = DATA_INPUT_LENGTH*turn;
+  const int value_size = DATA_INPUT_LENGTH * turn;
   uint8_t *values = new uint8_t[value_size * n]{};
   // for (int i = 0; i < value_size * n; ++i) {
   //   values[i] = dist(g);
   // }
-  for (int i = 0; i < value_size; ++i) {
+  for (int i = 0; i < value_size; ++i)
+  {
     values[i] = dist(g);
   }
-  for (int i = value_size; i < value_size*n; i++)
+  for (int i = value_size; i < value_size * n; i++)
   {
-    values[i] = values[i%value_size];
+    values[i] = values[i % value_size];
   }
-  
+
   values_bytes = values;
   value_indexs = new int[n]{};
   printf("finish generating values\n");
-  for (int i = 0; i < n+1; i++){
-    value_indexs[i] = value_size/8;
+  for (int i = 0; i < n + 1; i++)
+  {
+    value_indexs[i] = value_size / 8;
   }
 }
 
-void data_gen(const uint8_t* input, const uint8_t *& batch_data, int *&value_indexs, int n, int turn){
-  const int value_size = DATA_INPUT_LENGTH*turn;
+void data_gen(const uint8_t *input, const uint8_t *&batch_data, int *&value_indexs, int n, int turn)
+{
+  const int value_size = DATA_INPUT_LENGTH * turn;
   uint8_t *values = new uint8_t[value_size * n]{};
   for (int i = 0; i < n; i++)
   {
     for (int j = 0; j < value_size; j++)
     {
-      values[i*value_size+j] = input[j];
+      values[i * value_size + j] = input[j];
     }
   }
   batch_data = values;
   value_indexs = new int[n]{};
   printf("finish generating values\n");
-  for (int i = 0; i < n+1; i++){
-    value_indexs[i] = value_size/8;
+  for (int i = 0; i < n + 1; i++)
+  {
+    value_indexs[i] = value_size / 8;
   }
 }
 
-int main(){
-  const uint8_t * h_data = nullptr;
-  int * indexs = nullptr;
-  uint64_t * hash = new uint64_t[GEN_DATA_MUL*GEN_DATA_NUM*4];
-  const uint8_t *input = reinterpret_cast<const uint8_t *>("hgfcghvbjk829198sxdrftgyhjhgftygysdjfhusaid0sadkowq9edi0qwoej2189736ey781dhj289ikj982ujhr3w1ksw0i9sdwq-0ejd30qowkdx-0q0owl09ei90123u198ewd3jwq89de90qdjuqhrfduiwqjesuiedhgr3q78yeh879217jes89kqwspGHjasdhfiujsabhnfjidsnfjinewionfiuwehbnfuidsnijfnsdijnfidsjq0w9ie9012e2cdandiujnqwiudn9iasuondcioasjnmdiojasnmdoikjasnmdiojksandiujqnaiuwnqiudnasiujnxcijanmomsomdaopdjmkiowniudnasoidjiowjhquireyguiqwnbfudmioasmcoiadmnciuasdhduiwsahdiuwqnbduiasnmcasod0ajw90qokd0qwjd098hjrqfijwodiqjwudh28971hed89uwhqnd789uqwhdnbucsaijnsauhdbauihwjsbduiqwnbduiwqnuxcsaudhiuqwduw3q2eh2918qed98iqwsjnmdisadcqainjiwuyhe8721uqidnwqadoasmcxiaohjew398i1qdwisacasioedrxdtcvbnvjghfgkkhvgcfgtdxfghjkbvgcfdtxresxtfyghkjhgvcfdxtcghjklnhbvgcfxdxtrfyghjbvgcfxdtfyguhijkbvgcfxdyguhjkbvgcfxdtfyughjbvgcfxdtryfughjbvgcfdtrftyughjbvgcftdrfyughijbvgcfdrttyughibjvgcfdr5t6y8iuhbjvgcfydr57t6uygibhjvgchfydrft");
-  
-  data_gen(input, h_data, indexs, GEN_DATA_NUM*GEN_DATA_MUL, MUL_FACTOR);
-  
+int main()
+{
+  const uint8_t *h_data = nullptr;
+  int *indexs = nullptr;
+  uint64_t *hash = new uint64_t[GEN_DATA_MUL * GEN_DATA_NUM * 4];
+  const uint8_t *input = reinterpret_cast<const uint8_t *>("asydasuydhsuabncuabuaxbcnisuqwec");
+  data_gen(input, h_data, indexs, GEN_DATA_NUM * GEN_DATA_MUL, MUL_FACTOR);
+
   GPUHashMultiThread::load_constants();
 
   perf::CpuTimer<perf::ns> batch_timer;
-  call_keccak_batch_kernel((uint64_t*)h_data, indexs, hash, batch_timer, GEN_DATA_MUL*GEN_DATA_NUM);
-  uint8_t* hash8 =(uint8_t*)hash;
-  for (size_t i = 0; i < GEN_DATA_MUL*GEN_DATA_NUM; i++)
+  call_keccak_batch_kernel((uint64_t *)h_data, indexs, hash, batch_timer, GEN_DATA_MUL * GEN_DATA_NUM);
+  uint8_t *hash8 = (uint8_t *)hash;
+  for (size_t i = 0; i < GEN_DATA_MUL * GEN_DATA_NUM; i++)
   {
     for (int j = 0; j < 32; j++)
     {
-      printf("%d:%x ",j, hash8[i*32+j]);
+      printf("%d:%x ", j, hash8[i * 32 + j]);
     }
     printf("\n");
   }
-  
+
   printf("GPU batch time: %d ns \n", batch_timer.get());
 
   return 0;
--- test/hash_test.cu
+++ test/hash_test.cu
@@ -6,7 +6,7 @@
 #include <random>
 #include <algorithm>
 
-#define DATA_INPUT_LENGTH 1024
+#define DATA_INPUT_LENGTH 32
 
 void call_keccak_basic_kernel(const uint8_t *in, uint32_t data_byte_len, uint8_t *out)
 {
@@ -18,12 +18,12 @@ void call_keccak_basic_kernel(const uint8_t *in, uint32_t data_byte_len, uint8_t
   GPUHashMultiThread::load_constants();
   CUDA_SAFE_CALL(cudaMalloc(&d_data, input_size64 * sizeof(uint64_t)));
   CUDA_SAFE_CALL(cudaMalloc(&out_hash, 4 * sizeof(uint64_t)));
-  CUDA_SAFE_CALL(cudaMemset(out_hash, 0, 4*sizeof(uint64_t)));
+  CUDA_SAFE_CALL(cudaMemset(out_hash, 0, 4 * sizeof(uint64_t)));
   CUDA_SAFE_CALL(cudaMemset(d_data, 0, input_size64));
   CUDA_SAFE_CALL(cudaMemcpy((uint8_t *)d_data, in, data_byte_len, cudaMemcpyHostToDevice));
   keccak_kernel<<<1, 32>>>(d_data, out_hash, data_byte_len * 8);
   CUDA_SAFE_CALL(cudaDeviceSynchronize());
-  CUDA_SAFE_CALL(cudaMemcpy(out, (uint8_t*)out_hash, HASH_SIZE, cudaMemcpyDeviceToHost));
+  CUDA_SAFE_CALL(cudaMemcpy(out, (uint8_t *)out_hash, HASH_SIZE, cudaMemcpyDeviceToHost));
   CUDA_SAFE_CALL(cudaFree(d_data));
   CUDA_SAFE_CALL(cudaFree(out_hash));
 }
@@ -53,27 +53,27 @@ void data_gen(const uint8_t *&values_bytes, int *&value_indexs, int n, int turn)
   }
 }
 
-
-int main() {
+int main()
+{
   uint8_t *hash;
   hash = (uint8_t *)malloc(32 * sizeof(uint8_t));
   memset(hash, 0, 32);
-  const uint8_t *input = reinterpret_cast<const uint8_t *>("hgfcghvbjk8291982cisacasioedrxdtcvbnvjghfgkkhvgcfgtdxfghjkbvgcfdtxresxtfyghkjhgvcfdxtcghjklnhbvgcfxdxtrfyghjbvgcfxdtfyguhijkbvgcfxdyguhjkbvgcfxdtfyughjbvgcfxdtryfughjbvgcfdtrftyughjbvgcftdrfyughijbvgcfdrttyughibjvgcfdr5t6y8iuhbjvgcfydr57t6uygibhjvgchfydrft");
-  CPUHash::calculate_hash(input, 256, hash);
+  const uint8_t *input = reinterpret_cast<const uint8_t *>("asydasuydhsuabncuabuaxbcnisuqwec");
+  CPUHash::calculate_hash(input, 32, hash);
   util::println_hex(hash, 32);
 
   memset(hash, 0, 32);
 
   uint8_t *device_input;
   uint8_t *device_hash;
-  CUDA_SAFE_CALL(gutil::DeviceAlloc(device_input, 256));
+  CUDA_SAFE_CALL(gutil::DeviceAlloc(device_input, 32));
   CUDA_SAFE_CALL(gutil::DeviceAlloc(device_hash, 32));
-  CUDA_SAFE_CALL(gutil::DeviceSet(device_input, 0, 256));
+  CUDA_SAFE_CALL(gutil::DeviceSet(device_input, 0, 32));
   CUDA_SAFE_CALL(gutil::DeviceSet(device_hash, 0, 32));
-  CUDA_SAFE_CALL(gutil::CpyHostToDevice(device_input, input, 256));
+  CUDA_SAFE_CALL(gutil::CpyHostToDevice(device_input, input, 32));
 
   GPUHashSingleThread::load_constants();
-  GPUHashSingleThread::test_calculate_hash<<<1, 1>>>(device_input, 256,
+  GPUHashSingleThread::test_calculate_hash<<<1, 1>>>(device_input, 32,
                                                      device_hash);
   CUDA_SAFE_CALL(cudaDeviceSynchronize());
   CUDA_SAFE_CALL(gutil::CpyDeviceToHost(hash, device_hash, 32));
@@ -81,7 +81,7 @@ int main() {
   util::println_hex(hash, 32);
 
   memset(hash, 0, 32);
-  call_keccak_basic_kernel(input, 256, hash);
+  call_keccak_basic_kernel(input, 32, hash);
   util::println_hex(hash, 32);
 
   CUDA_SAFE_CALL(gutil::DeviceFree(device_hash));
