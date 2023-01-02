59d01afb2d9f20ea7d33c6dbe38b966d0979c417
yangshen <yangshen.d@outlook.com>
2023-01-02 14:33:46 +0800

    fix download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -2,15 +2,15 @@
 mkdir -p dataset && cd dataset
 mkdir -p wiki && cd wiki
 mkdir -p index && mkdir -p value
-if [ ! -f 'value/enwiki-20220920-pages-articles-multistream1.xml-p1p41242' ];
+if [ ! -f 'value/enwiki-20230101-pages-articles-multistream1.xml-p1p41242' ];
 then
-  wget https://dumps.wikimedia.org/enwiki/20220920/enwiki-20220920-pages-articles-multistream1.xml-p1p41242.bz2
-  bzip2 -d enwiki-20220920-pages-articles-multistream-index1.txt-p1p41242.bz2
+  wget https://dumps.wikimedia.org/enwiki/20230101/enwiki-20230101-pages-articles-multistream1.xml-p1p41242.bz2
+  bzip2 -d enwiki-20230101-pages-articles-multistream1.xml-p1p41242.bz2
 fi
-if [ ! -f 'index/enwiki-20220920-pages-articles-multistream-index1.txt-p1p41242' ];
+if [ ! -f 'index/enwiki-20230101-pages-articles-multistream-index1.txt-p1p41242' ];
 then
-  wget https://dumps.wikimedia.org/enwiki/20220920/enwiki-20220920-pages-articles-multistream-index1.txt-p1p41242.bz2
-  bzip2 -d enwiki-20220920-pages-articles-multistream1.xml-p1p41242.bz2
+  wget https://dumps.wikimedia.org/enwiki/20230101/enwiki-20230101-pages-articles-multistream-index1.txt-p1p41242.bz2
+  bzip2 -d enwiki-20230101-pages-articles-multistream-index1.txt-p1p41242.bz2
 fi
-mv enwiki-20220920-pages-articles-multistream-index1.txt-p1p41242 index/
-mv enwiki-20220920-pages-articles-multistream1.xml-p1p41242 value/
+mv enwiki-20230101-pages-articles-multistream-index1.txt-p1p41242 index/
+mv enwiki-20230101-pages-articles-multistream1.xml-p1p41242 value/
