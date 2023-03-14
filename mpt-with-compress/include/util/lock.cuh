5eb95705025d1519bbf43ddc81412f2d66da10e4
TKONIY <yangshen.d@outlook.com>
2023-03-14 07:35:51 +0000

    1. Finish btree-olc 2. Update atomic_load and atomic_store with template

--- mpt-with-compress/include/util/lock.cuh
+++ mpt-with-compress/include/util/lock.cuh
@@ -21,18 +21,20 @@ __device__ void release_lock(int *lock) {
 // atomic load/store
 using ull_t = unsigned long long;
 // addr must be aligned properly.
-__device__ __forceinline__ unsigned int atomic_load(const ull_t *addr) {
-  const volatile ull_t *vaddr = addr;  // volatile to bypass cache
+template<typename T>
+__device__ __forceinline__ T atomic_load(const T *addr) {
+  const volatile T *vaddr = addr;  // volatile to bypass cache
   __threadfence();  // for seq_cst loads. Remove for acquire semantics.
-  const unsigned int value = *vaddr;
+  const T value = *vaddr;
   // fence to ensure that dependent reads are correctly ordered
   __threadfence();
   return value;
 }
 
 // addr must be aligned properly.
-__device__ __forceinline__ void atomic_store(ull_t *addr, ull_t value) {
-  volatile ull_t *vaddr = addr;  // volatile to bypass cache
+template<typename T>
+__device__ __forceinline__ void atomic_store(T *addr, T value) {
+  volatile T *vaddr = addr;  // volatile to bypass cache
   // fence to ensure that previous non-atomic stores are visible to other
   // threads
   __threadfence();
